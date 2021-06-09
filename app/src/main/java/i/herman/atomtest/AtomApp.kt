f0855b372b55a0bbc27eabf1b7b97faf0038c205
Illia Herman <illiaherman8@gmail.com>
2021-06-09 13:11:52 +0200

    init commit

---
+++ app/src/main/java/i/herman/atomtest/AtomApp.kt
@@ -0,0 +1,11 @@
+package i.herman.atomtest
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Illia Herman on 09.06.2021.
+ */
+@HiltAndroidApp
+class AtomApp : Application() {
+}
---
+++ app/src/main/java/i/herman/atomtest/presentation/activity/MainActivity.kt
@@ -0,0 +1,15 @@
+package i.herman.atomtest.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import i.herman.atomtest.R
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
